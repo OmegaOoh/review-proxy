13941b85aab34d686b7d917e21a8dfd2df18419b
Sirapob PHASUT <omegaooh@gmail.com>
2026-04-13 15:04:49 +0700

    refactor: introduce extention module to make service easier to maintain

---
+++ src/services/syncing/Extensions/MessagingExtensions.cs
@@ -0,0 +1,36 @@
+using MassTransit;
+using ReviewProxy.Contracts;
+using Syncing.Services;
+
+namespace Syncing.Extensions;
+
+public static class MessagingExtensions
+{
+    public static IServiceCollection AddSyncingMessaging(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMassTransit(options =>
+        {
+            options.AddConsumer<IssueApprovalConsumer>();
+
+            options.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host(new Uri(configuration["MassTransit:Host"] ?? "rabbitmq://localhost:5672"), h =>
+                {
+                    h.Username(configuration["MassTransit:Username"] ?? "guest");
+                    h.Password(configuration["MassTransit:Password"] ?? "guest");
+                });
+
+                cfg.Message<IssueApprovalEvent>(e => e.SetEntityName("issue-approval-exchange"));
+
+                cfg.ReceiveEndpoint("syncing-service-issue-approval", e =>
+                {
+                    e.ConfigureConsumer<IssueApprovalConsumer>(context);
+                });
+
+                cfg.ConfigureEndpoints(context);
+            });
+        });
+
+        return services;
+    }
+}
---
+++ src/services/syncing/Extensions/SyncingServiceExtensions.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using Syncing.Interfaces;
+using Syncing.Services;
+
+namespace Syncing.Extensions;
+
+public static class SyncingServiceExtensions
+{
+    public static IServiceCollection AddSyncingInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOpenApi();
+        services.AddHealthChecks();
+
+        services.Configure<ForwardedHeadersOptions>(options =>
+        {
+            options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost;
+            options.KnownIPNetworks.Clear();
+            options.KnownProxies.Clear();
+            options.ForwardLimit = null;
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddSyncingHttpClients(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHttpClient("identity", client =>
+        {
+            client.BaseAddress = new Uri(configuration["Services:Identity"] ?? "http://identity");
+            var secret = configuration["Services:InternalSecret"];
+            if (!string.IsNullOrWhiteSpace(secret))
+                client.DefaultRequestHeaders.Add("X-Internal-Secret", secret);
+        });
+
+        services.AddHttpClient("github", client =>
+        {
+            client.DefaultRequestHeaders.Add("User-Agent", "ReviewProxy");
+            client.DefaultRequestHeaders.Add("X-GitHub-Api-Version", "2026-03-10");
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddSyncingBusinessServices(this IServiceCollection services)
+    {
+        services.AddScoped<IGitHubClientFactory, GitHubClientFactory>();
+        services.AddScoped<IIdentityClient, IdentityClient>();
+        services.AddScoped<IGitHubInstallationService, GitHubInstallationService>();
+        services.AddScoped<IGitHubService, GitHubService>();
+        services.AddScoped<ISyncingService, SyncingService>();
+        return services;
+    }
+}
